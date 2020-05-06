0daf2f6f9a15aed4fabdb4cd590be7befcb8d250
Nathan Bosch <nathangerbenbosch@gmail.com>
2020-05-06 12:57:18 +0200

    add sounds!

--- notification_sound_implementation/js/notif.js
+++ notification_sound_implementation/js/notif.js
@@ -7,22 +7,32 @@ class Stats {
   }
 }
 
+class Sound {
+  constructor(notifurl, texturl) {
+    this.notif = notifurl;
+    this.text = texturl;
+  }
+}
+
 class Scene {
-  constructor(bimg, stats, instruction) {
+  constructor(bimg, stats, instruction, sound) {
     this.bimg = bimg;
     this.stats = stats;
     this.instruction = instruction;
+    this.sound = sound;
   }
 }
 
 let currentScene = 0;
 let colorBlindMode = false;
+const sound1 = new Sound('notifs/n1', 'spoken/n1');
+const sound2 = new Sound('notifs/n2', 'spoken/n2');
 
 const stats1 = new Stats("80 km/h", "14:30", "Groningen", "20 minutes")
 const stats2 = new Stats("35 km/h", "09:20", "Assen", "5 minutes")
 let scenes = [];
-const scene1 = new Scene('bimgs/bg0', stats1, "Take the first exit");
-const scene2 = new Scene('bimgs/bg1', stats2, "Take the second exit");
+const scene1 = new Scene('bimgs/bg0', stats1, "Turn left", sound1);
+const scene2 = new Scene('bimgs/bg1', stats2, "Turn right", sound2);
 scenes.push(scene1);
 scenes.push(scene2);
 
@@ -63,4 +73,9 @@ function loadScene(index) {
 
 function updateSettings() {
   loadScene(currentScene);
+}
+
+function playSound() {
+  new Audio(scenes[currentScene].sound.notif + '.mp3').play();
+  new Audio(scenes[currentScene].sound.text + '.mp3').play();
 }

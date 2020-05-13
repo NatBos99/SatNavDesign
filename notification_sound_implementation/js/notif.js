c72c3204bf313c1188b564a7f3e26c8c57a1d0ef
Nathan Bosch <nathangerbenbosch@gmail.com>
2020-05-13 16:37:06 +0200

    Adds notification sounds, changes images, etc...

--- notification_sound_implementation/js/notif.js
+++ notification_sound_implementation/js/notif.js
@@ -1,9 +1,8 @@
 class Stats {
-  constructor(speed, time, destination, time_left) {
-    this.speed = speed;
+  constructor(time, destination, eta) {
     this.time = time;
     this.destination = destination;
-    this.time_left = time_left;
+    this.eta = eta;
   }
 }
 
@@ -25,16 +24,31 @@ class Scene {
 
 let currentScene = 0;
 let colorBlindMode = false;
+let currentVolume = 50;
+let currentPitch = "medium";
+let speakerGender = "male";
 const sound1 = new Sound('notifs/n1', 'spoken/n1');
-const sound2 = new Sound('notifs/n2', 'spoken/n2');
+const sound2 = new Sound('notifs/n1', 'spoken/n2');
+const sound3 = new Sound('notifs/n3', 'spoken/n3');
+const sound4 = new Sound('notifs/n4', 'spoken/n4');
+const sound5 = new Sound('notifs/n5', 'spoken/n5');
 
-const stats1 = new Stats("80 km/h", "14:30", "Groningen", "20 minutes")
-const stats2 = new Stats("35 km/h", "09:20", "Assen", "5 minutes")
+const stats1 = new Stats("13:50", "Wall Street", "13:56")
+const stats2 = new Stats("13:50", "Wall Avenue", "14:30")
+const stats3 = new Stats("09:00", "Short Street", "09:30")
+const stats4 = new Stats("13:50", "Long Drive", "14:30")
+const stats5 = new Stats("15:20", "Central Park", "19:30")
 let scenes = [];
-const scene1 = new Scene('bimgs/bg0', stats1, "Turn left", sound1);
-const scene2 = new Scene('bimgs/bg1', stats2, "Turn right", sound2);
+const scene1 = new Scene('bimgs/bg0', stats1, "Take the first left", sound1);
+const scene2 = new Scene('bimgs/bg1', stats2, "Take the first right", sound2);
+const scene3 = new Scene('bimgs/bg2', stats3, "Take the third exit at the roundabout", sound3);
+const scene4 = new Scene('bimgs/bg3', stats4, "Take the first exit in 200 meters", sound4);
+const scene5 = new Scene('bimgs/bg4', stats5, "Missed exit. Recalculating.", sound5);
 scenes.push(scene1);
 scenes.push(scene2);
+scenes.push(scene3);
+scenes.push(scene4);
+scenes.push(scene5);
 
 $(document).ready(function () {
   loadScene(currentScene);
@@ -45,6 +59,20 @@ $(document).ready(function () {
   changeFontSize("medium");
   $('input[name=fontsizeoptions]').click(function () {
     changeFontSize(this.value);
+  });
+  $('input[name=pitchshift]').click(function () {
+    currentPitch = this.value;
+  });
+  $('input[name=speakergender]').click(function () {
+    speakerGender = this.value;
+  });
+  const volumeSlider = $('#volumeslider');
+  const pitchSlider = $('#pitchslider');
+  volumeSlider.on('input change', () => {
+    currentVolume = volumeSlider.val();
+  });
+  pitchSlider.on('input change', () => {
+    currentPitch = pitchSlider.val();
   });
 });
 
@@ -59,16 +87,19 @@ function changeSceneLeft() {
 function changeFontSize(fontSize) {
   if (fontSize == "small") {
     $('.instructions').css('font-size', '160%');
-    $('.instructions').css('margin-top', '4%');
+    $('.instructions').css('margin-top', '3%');
     $('.drive_statistics').css('font-size', '160%');
+    $('.drive_statistics').css('margin-top', '1%');
   } else if (fontSize == "medium") {
     $('.instructions').css('font-size', '200%');
-    $('.instructions').css('margin-top', '3%');
+    $('.instructions').css('margin-top', '2%');
     $('.drive_statistics').css('font-size', '200%');
+    $('.drive_statistics').css('margin-top', '0%');
   } else if (fontSize == "large") {
     $('.instructions').css('font-size', '240%');
-    $('.instructions').css('margin-top', '2%');
+    $('.instructions').css('margin-top', '1%');
     $('.drive_statistics').css('font-size', '240%');
+    $('.drive_statistics').css('margin-top', '-1%');
   } else {
     console.log("Invalid font size: " + fontSize);
   }
@@ -82,6 +113,9 @@ function loadScene(index) {
   }
   let scene = scenes[index];
   $(".instructions").children("span")[0].innerHTML = scene.instruction;
+  $(".destination").children("span")[1].innerHTML = scene.stats.destination;
+  $(".time").children("span")[1].innerHTML = scene.stats.time;
+  $(".eta").children("span")[1].innerHTML = scene.stats.eta;
   if (colorBlindMode) {
     $(".satnav").css("background-image", "url('" + scene.bimg + "cb.png')")
   } else {
@@ -89,7 +123,7 @@ function loadScene(index) {
   }
   $(".satnav").css("background-position", "0% 30%")
   $(".satnav").css("background-repeat", "no-repeat")
-  $(".satnav").css("background-size", "100% 75%")
+  $(".satnav").css("background-size", "100% 80%")
   currentScene = index;
 }
 
@@ -98,6 +132,10 @@ function updateSettings() {
 }
 
 function playSound() {
-  new Audio(scenes[currentScene].sound.notif + '.mp3').play();
-  new Audio(scenes[currentScene].sound.text + '.mp3').play();
+  let a1 = new Audio(scenes[currentScene].sound.notif + currentPitch + '.mp3');
+  a1.volume = currentVolume / 100;
+  a1.play();
+  let a2 = new Audio(scenes[currentScene].sound.text + speakerGender + currentPitch + '.mp3');
+  a2.volume = currentVolume / 100;
+  a2.play();
 }
